a08dec004d052e936b2e24c21314e8e00c8e718b
furkanogutcu <56116207+furkanogutcu@users.noreply.github.com>
2021-08-12 15:19:49 +0300

    Created car Data Transformation Object.

---
+++ Entities/DTOs/CarDetailDto.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Core.Entities;
+
+namespace Entities.DTOs
+{
+    public class CarDetailDto:IDto
+    {
+        public int Id { get; set; }
+        public string BrandName { get; set; }
+        public string ColorName { get; set; }
+        public DateTime ModelYear { get; set; }
+        public decimal DailyPrice { get; set; }
+        public string Description { get; set; }
+    }
+}
