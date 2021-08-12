2cd6177fde101a7cd729204820eecd60e6c095b6
furkanogutcu <56116207+furkanogutcu@users.noreply.github.com>
2021-08-12 13:23:01 +0300

    Core layer used

--- DataAccess/Concrete/EntityFramework/EfColorDal.cs
+++ DataAccess/Concrete/EntityFramework/EfColorDal.cs
@@ -4,60 +4,14 @@ using System.Linq;
 using System.Linq.Expressions;
 using System.Text;
 using System.Threading.Tasks;
+using Core.DataAccess.EntityFramework;
 using DataAccess.Abstract;
 using Entities.Concrete;
 using Microsoft.EntityFrameworkCore;
 
 namespace DataAccess.Concrete.EntityFramework
 {
-    public class EfColorDal:IColorDal
+    public class EfColorDal:EfEntityRepositoryBase<Color,RentACarContext>,IColorDal
     {
-        public List<Color> GetAll(Expression<Func<Color, bool>> filter = null)
-        {
-            using (RentACarContext context = new RentACarContext())
-            {
-                return filter == null
-                    ? context.Set<Color>().ToList()
-                    : context.Set<Color>().Where(filter).ToList();
-            }
-        }
-
-        public Color Get(Expression<Func<Color, bool>> filter)
-        {
-            using (RentACarContext context = new RentACarContext())
-            {
-                return context.Set<Color>().SingleOrDefault(filter);
-            }
-        }
-
-        public void Add(Color entity)
-        {
-            using (RentACarContext context = new RentACarContext())
-            {
-                var addedColor = context.Entry(entity);
-                addedColor.State = EntityState.Added;
-                context.SaveChanges();
-            }
-        }
-
-        public void Update(Color entity)
-        {
-            using (RentACarContext context = new RentACarContext())
-            {
-                var updatedColor = context.Entry(entity);
-                updatedColor.State = EntityState.Modified;
-                context.SaveChanges();
-            }
-        }
-
-        public void Delete(Color entity)
-        {
-            using (RentACarContext context = new RentACarContext())
-            {
-                var deletedColor = context.Entry(entity);
-                deletedColor.State = EntityState.Deleted;
-                context.SaveChanges();
-            }
-        }
     }
 }
