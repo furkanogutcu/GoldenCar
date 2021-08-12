2cd8c9c9f873bd19a6daec19e8ed690afa0ab83c
furkanogutcu <56116207+furkanogutcu@users.noreply.github.com>
2021-08-12 15:22:04 +0300

    GetCarDetails method served.

--- Business/Abstract/ICarService.cs
+++ Business/Abstract/ICarService.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Entities.Concrete;
+using Entities.DTOs;
 
 namespace Business.Abstract
 {
@@ -16,5 +17,6 @@ namespace Business.Abstract
         void Delete(Car car);
         List<Car> GetCarsByBrandId(int id);
         List<Car> GetCarsByColorId(int id);
+        List<CarDetailDto> GetCarDetails();
     }
 }
--- Business/Concrete/CarManager.cs
+++ Business/Concrete/CarManager.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 using Business.Abstract;
 using DataAccess.Abstract;
 using Entities.Concrete;
+using Entities.DTOs;
 
 namespace Business.Concrete
 {
@@ -54,6 +55,11 @@ namespace Business.Concrete
         public List<Car> GetCarsByColorId(int id)
         {
             return _carDal.GetAll(c => c.ColorId == id);
+        }
+
+        public List<CarDetailDto> GetCarDetails()
+        {
+            return _carDal.GetCarDetails();
         }
     }
 }
