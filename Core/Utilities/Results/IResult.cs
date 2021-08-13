6009d5e95eab57deec4c519c3f4d049f9f5b1d2f
furkanogutcu <56116207+furkanogutcu@users.noreply.github.com>
2021-08-13 14:57:44 +0300

    Result structure added.

---
+++ Core/Utilities/Results/IResult.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Utilities.Results
+{
+    //This type can be used for void
+    public interface IResult
+    {
+        bool Success { get; }
+        string Message { get; }
+    }
+}
