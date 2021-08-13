2e3a331aff52535906e939430552703e06fd609e
furkanogutcu <56116207+furkanogutcu@users.noreply.github.com>
2021-08-14 00:45:34 +0300

    Added DTO of new entities

---
+++ Entities/DTOs/CustomerDetailDto.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Core.Entities;
+
+namespace Entities.DTOs
+{
+    public class CustomerDetailDto:IDto
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string CompanyName { get; set; }
+    }
+}
---
+++ Entities/DTOs/RentalDetailDto.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Core.Entities;
+
+namespace Entities.DTOs
+{
+    public class RentalDetailDto:IDto
+    {
+        public int Id { get; set; }
+        public decimal DailyPrice { get; set; }
+        public DateTime? RentDate { get; set; }
+        public DateTime? ReturnDate { get; set; }
+
+    }
+}
