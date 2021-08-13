6f0a3d3977ef3008b9e0da007f04dd1c0c9c4c93
furkanogutcu <56116207+furkanogutcu@users.noreply.github.com>
2021-08-14 00:38:23 +0300

    New entities added

---
+++ Entities/Concrete/Customer.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Core.Entities;
+
+namespace Entities.Concrete
+{
+    public class Customer:IEntity
+    {
+        public int Id { get; set; }
+        public int UserId { get; set; }
+        public string CompanyName { get; set; }
+    }
+}
---
+++ Entities/Concrete/Rental.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Core.Entities;
+
+namespace Entities.Concrete
+{
+    public class Rental:IEntity
+    {
+        public int Id { get; set; }
+        public int CarId { get; set; }
+        public int CustomerId { get; set; }
+        public DateTime? RentDate { get; set; } //Datetime = Values cannot be null -- Datetime? = Nullable Datetime (Values can be null)
+        public DateTime? ReturnDate { get; set; }
+    }
+}
