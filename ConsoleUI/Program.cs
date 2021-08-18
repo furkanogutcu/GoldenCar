a24c5b51d61aec3c2be2d3f1a8f5aa0d43bbd564
furkanogutcu <56116207+furkanogutcu@users.noreply.github.com>
2021-08-18 17:56:55 +0300

    ConsoleUI testi reconfigured

--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -17,7 +17,6 @@ namespace ConsoleUI
 
             CarManager carManager = new CarManager(new EfCarDal());
             CarManagerAddTest(carManager);
-            ListCars(carManager);
 
             UserManager userManager = new UserManager(new EfUserDal());
             UserManagerAddTest(userManager);
@@ -27,73 +26,78 @@ namespace ConsoleUI
 
             RentalManager rentalManager = new RentalManager(new EfRentalDal());
             RentACarTest(rentalManager);
-            RentACarTest(rentalManager);
 
+            Console.WriteLine("Test successful!");
             Console.ReadLine();
         }
 
         private static void RentACarTest(RentalManager rentalManager)
         {
-            var x = rentalManager.Add(new Rental {CarId = 4, CustomerId = 1, RentDate = DateTime.Now});
-            Console.WriteLine("{0} ---> {1}",x.Success,x.Message);
+            rentalManager.Add(new Rental { CarId = 4, CustomerId = 6, RentDate = DateTime.Now });
+            rentalManager.Add(new Rental { CarId = 6, CustomerId = 2, RentDate = DateTime.Now });
+            rentalManager.Add(new Rental { CarId = 1, CustomerId = 3, RentDate = DateTime.Now });
+            rentalManager.Add(new Rental { CarId = 3, CustomerId = 1, RentDate = DateTime.Now });
+            rentalManager.Add(new Rental { CarId = 5, CustomerId = 2, RentDate = DateTime.Now });
         }
 
         private static void CustomerManagerAddTest(CustomerManager customerManager)
         {
-            customerManager.Add(new Customer {UserId = 1, CompanyName = "example1 Company"});
-            customerManager.Add(new Customer {UserId = 3, CompanyName = "example2 Company"});
+            customerManager.Add(new Customer { UserId = 1, CompanyName = "Test Company 1" });
+            customerManager.Add(new Customer { UserId = 4, CompanyName = "Test Company 2" });
+            customerManager.Add(new Customer { UserId = 2, CompanyName = "Test Company 3" });
+            customerManager.Add(new Customer { UserId = 7, CompanyName = "Test Company 4" });
+            customerManager.Add(new Customer { UserId = 6, CompanyName = "Test Company 5" });
+            customerManager.Add(new Customer { UserId = 10, CompanyName = "Test Company 6" });
         }
 
         private static void UserManagerAddTest(UserManager userManager)
         {
-            userManager.Add(new User {FirstName = "Furkan", LastName = "Ogutcu", Email = "example@", Password = "123456"});
-            userManager.Add(new User {FirstName = "test1", LastName = "test2", Email = "example2@", Password = "654321"});
-            userManager.Add(new User {FirstName = "test3", LastName = "test4", Email = "example2@", Password = "654321"});
-        }
-
-        private static void ListCars(CarManager carManager)
-        {
-            foreach (var car in carManager.GetCarDetails().Data)
-            {
-                Console.WriteLine("ID: {0} , Brand: {1} , Color: {2} , ModelYear: {3} , DailyPrice {4} , Description: {5}",
-                    car.Id, car.BrandName, car.ColorName,
-                    car.ModelYear.Year, car.DailyPrice, car.Description);
-            }
+            userManager.Add(new User { FirstName = "TestName1", LastName = "TestSurname1", Email = "test1@.com", Password = "123456789" });
+            userManager.Add(new User { FirstName = "TestName2", LastName = "TestSurname2", Email = "test2@.com", Password = "987654321" });
+            userManager.Add(new User { FirstName = "TestName3", LastName = "TestSurname3", Email = "test3@.com", Password = "123456789" });
+            userManager.Add(new User { FirstName = "TestName4", LastName = "TestSurname4", Email = "test4@.com", Password = "987654321" });
+            userManager.Add(new User { FirstName = "TestName5", LastName = "TestSurname5", Email = "test5@.com", Password = "123456789" });
+            userManager.Add(new User { FirstName = "TestName6", LastName = "TestSurname6", Email = "test6@.com", Password = "987654321" });
+            userManager.Add(new User { FirstName = "TestName7", LastName = "TestSurname7", Email = "test7@.com", Password = "123456789" });
+            userManager.Add(new User { FirstName = "TestName8", LastName = "TestSurname8", Email = "test8@.com", Password = "987654321" });
+            userManager.Add(new User { FirstName = "TestName9", LastName = "TestSurname9", Email = "test9@.com", Password = "123456789" });
+            userManager.Add(new User { FirstName = "TestName10", LastName = "TestSurname10", Email = "test10@.com", Password = "987654321" });
         }
 
         private static void CarManagerAddTest(CarManager carManager)
         {
-            carManager.Add(new Car{BrandId = 1, ColorId = 3, ModelYear = new DateTime(2010, 1, 1), DailyPrice = 500, Description = "1.2 liter engine"});
-            carManager.Add(new Car{BrandId = 1, ColorId = 2, ModelYear = new DateTime(2015, 1, 1), DailyPrice = 750, Description = "1.4 liter engine"});
-            carManager.Add(new Car{BrandId = 2, ColorId = 7, ModelYear = new DateTime(2012, 1, 1), DailyPrice = 900, Description = "1.4 liter engine"});
-            carManager.Add(new Car{BrandId = 3, ColorId = 4, ModelYear = new DateTime(2021, 1, 1), DailyPrice = 1500, Description = "1.6 liter engine"});
-            carManager.Add(new Car{BrandId = 5, ColorId = 4, ModelYear = new DateTime(2017, 1, 1), DailyPrice = 600, Description = "1.6 liter engine"});
-            carManager.Add(new Car{BrandId = 3, ColorId = 5, ModelYear = new DateTime(2018, 1, 1), DailyPrice = 900, Description = "1.2 liter engine"});
-            carManager.Add(new Car{BrandId = 3, ColorId = 2, ModelYear = new DateTime(2021, 1, 1), DailyPrice = 1000, Description = "1.4 liter engine"});
-            carManager.Add(new Car{BrandId = 1, ColorId = 2, ModelYear = new DateTime(2014, 1, 1), DailyPrice = 1500, Description = "1.6 liter engine"});
-            carManager.Add(new Car{BrandId = 2, ColorId = 3, ModelYear = new DateTime(2021, 1, 1), DailyPrice = 3000, Description = "1.6 liter engine"});
-            carManager.Add(new Car{BrandId = 1, ColorId = 6, ModelYear = new DateTime(2015, 1, 1), DailyPrice = 1200, Description = "1.4 liter engine"});
-            carManager.Add(new Car{BrandId = 4, ColorId = 6, ModelYear = new DateTime(2000, 1, 1), DailyPrice = 1000, Description = "1.2 liter engine"});
+            carManager.Add(new Car { BrandId = 1, ColorId = 3, ModelYear = new DateTime(2010, 1, 1), DailyPrice = 500, Description = "1.2 liter engine" });
+            carManager.Add(new Car { BrandId = 1, ColorId = 2, ModelYear = new DateTime(2015, 1, 1), DailyPrice = 750, Description = "1.4 liter engine" });
+            carManager.Add(new Car { BrandId = 2, ColorId = 7, ModelYear = new DateTime(2012, 1, 1), DailyPrice = 900, Description = "1.4 liter engine" });
+            carManager.Add(new Car { BrandId = 3, ColorId = 4, ModelYear = new DateTime(2021, 1, 1), DailyPrice = 1500, Description = "1.6 liter engine" });
+            carManager.Add(new Car { BrandId = 5, ColorId = 4, ModelYear = new DateTime(2017, 1, 1), DailyPrice = 600, Description = "1.6 liter engine" });
+            carManager.Add(new Car { BrandId = 3, ColorId = 5, ModelYear = new DateTime(2018, 1, 1), DailyPrice = 900, Description = "1.2 liter engine" });
+            carManager.Add(new Car { BrandId = 3, ColorId = 2, ModelYear = new DateTime(2021, 1, 1), DailyPrice = 1000, Description = "1.4 liter engine" });
+            carManager.Add(new Car { BrandId = 1, ColorId = 2, ModelYear = new DateTime(2014, 1, 1), DailyPrice = 1500, Description = "1.6 liter engine" });
+            carManager.Add(new Car { BrandId = 2, ColorId = 3, ModelYear = new DateTime(2021, 1, 1), DailyPrice = 3000, Description = "1.6 liter engine" });
+            carManager.Add(new Car { BrandId = 1, ColorId = 6, ModelYear = new DateTime(2015, 1, 1), DailyPrice = 1200, Description = "1.4 liter engine" });
+            carManager.Add(new Car { BrandId = 4, ColorId = 1, ModelYear = new DateTime(2000, 1, 1), DailyPrice = 1000, Description = "1.2 liter engine" });
+            carManager.Add(new Car { BrandId = 2, ColorId = 1, ModelYear = new DateTime(2016, 1, 1), DailyPrice = 1700, Description = "1.4 liter engine" });
         }
 
         private static void ColorManagerAddTest(ColorManager colorManager)
         {
-            colorManager.Add(new Color {Name = "Black"});
-            colorManager.Add(new Color {Name = "Red"});
-            colorManager.Add(new Color {Name = "White"});
-            colorManager.Add(new Color {Name = "Yellow"});
-            colorManager.Add(new Color {Name = "Blue"});
-            colorManager.Add(new Color {Name = "Green"});
-            colorManager.Add(new Color {Name = "Metallic"});
+            colorManager.Add(new Color { Name = "Black" });
+            colorManager.Add(new Color { Name = "Red" });
+            colorManager.Add(new Color { Name = "White" });
+            colorManager.Add(new Color { Name = "Yellow" });
+            colorManager.Add(new Color { Name = "Blue" });
+            colorManager.Add(new Color { Name = "Green" });
+            colorManager.Add(new Color { Name = "Metallic" });
         }
 
         private static void BrandManagerAddTest(BrandManager brandManager)
         {
-            brandManager.Add(new Brand {Name = "BMW"});
-            brandManager.Add(new Brand {Name = "Mercedes"});
-            brandManager.Add(new Brand {Name = "Volkswagen"});
-            brandManager.Add(new Brand {Name = "Audi"});
-            brandManager.Add(new Brand {Name = "FIAT"});
+            brandManager.Add(new Brand { Name = "BMW" });
+            brandManager.Add(new Brand { Name = "Mercedes" });
+            brandManager.Add(new Brand { Name = "Volkswagen" });
+            brandManager.Add(new Brand { Name = "Audi" });
+            brandManager.Add(new Brand { Name = "FIAT" });
         }
     }
 }
