7ee21d9a7c24d228d7013c80b7498676137d59b1
furkanogutcu <56116207+furkanogutcu@users.noreply.github.com>
2021-09-02 00:58:38 +0300

    Add Transaction Scope
    This commit, adds Transaction Scope to the project. It uses this structure with Aspect.

---
+++ Core/Aspects/Autofac/Transaction/TransactionScopeAspect.cs
@@ -0,0 +1,26 @@
+﻿using System.Transactions;
+using Castle.DynamicProxy;
+using Core.Utilities.Interceptors;
+
+namespace Core.Aspects.Autofac.Transaction
+{
+    public class TransactionScopeAspect : MethodInterception
+    {
+        public override void Intercept(IInvocation invocation)
+        {
+            using (TransactionScope transactionScope = new TransactionScope())
+            {
+                try
+                {
+                    invocation.Proceed();
+                    transactionScope.Complete();
+                }
+                catch (System.Exception)
+                {
+                    transactionScope.Dispose();
+                    throw;
+                }
+            }
+        }
+    }
+}
