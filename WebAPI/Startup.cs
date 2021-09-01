f9537312e451393d871834eac298206bec8ce875
furkanogutcu <56116207+furkanogutcu@users.noreply.github.com>
2021-09-02 00:28:11 +0300

    Add CoreModule
    This commit, adds a structure to the project where we can inject global dependencies and uses this structure in WebAPI.

--- WebAPI/Startup.cs
+++ WebAPI/Startup.cs
@@ -1,10 +1,11 @@
+using Core.DependencyResolvers;
+using Core.Extensions;
 using Core.Utilities.IoC;
 using Core.Utilities.Security.Encryption;
 using Core.Utilities.Security.Jwt;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -26,8 +27,6 @@ namespace WebAPI
         {
             services.AddControllers();
 
-            services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
-
             var tokenOptions = Configuration.GetSection("TokenOptions").Get<TokenOptions>();
 
             services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
@@ -45,8 +44,10 @@ namespace WebAPI
                     };
                 });
 
-            ServiceTool.Create(services);
-
+            services.AddDependencyResolvers(new ICoreModule[]
+            {
+                new CoreModule()
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
