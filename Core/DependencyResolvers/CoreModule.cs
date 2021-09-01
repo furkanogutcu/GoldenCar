a76c5cedfe1900cb64dee6131951c72ea73c4c71
furkanogutcu <56116207+furkanogutcu@users.noreply.github.com>
2021-09-02 01:04:49 +0300

    Add Performance Test
    This commit adds Performance Testing to the project. It uses this structure with Aspect.
    
    It also injects the required dependency for Performance Testing in Core/DependencyResolvers/CoreModule.

--- Core/DependencyResolvers/CoreModule.cs
+++ Core/DependencyResolvers/CoreModule.cs
@@ -1,4 +1,5 @@
-﻿using Core.CrossCuttingConcerns.Caching;
+﻿using System.Diagnostics;
+using Core.CrossCuttingConcerns.Caching;
 using Core.CrossCuttingConcerns.Caching.Microsoft;
 using Core.Utilities.IoC;
 using Microsoft.AspNetCore.Http;
@@ -13,6 +14,7 @@ namespace Core.DependencyResolvers
             serviceCollection.AddMemoryCache();
             serviceCollection.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
             serviceCollection.AddSingleton<ICacheManager, MemoryCacheManager>();
+            serviceCollection.AddSingleton<Stopwatch>();
         }
     }
 }
