6ec35216392c9d88ed26ea20f034587f0b87a60d
furkanogutcu <56116207+furkanogutcu@users.noreply.github.com>
2021-09-08 16:01:49 +0300

    Add new property to CarDetailDto
    This commit, adds CarImages property to CarDetailDto. Along with this commitment, pictures of the car are sent to the client who requests the vehicle details from the API.

--- DataAccess/Concrete/EntityFramework/EfCarDal.cs
+++ DataAccess/Concrete/EntityFramework/EfCarDal.cs
@@ -9,29 +9,49 @@ using Entities.DTOs;
 
 namespace DataAccess.Concrete.EntityFramework
 {
-    public class EfCarDal:EfEntityRepositoryBase<Car,RentACarContext>,ICarDal
+    public class EfCarDal : EfEntityRepositoryBase<Car, RentACarContext>, ICarDal
     {
         public List<CarDetailDto> GetCarDetails(Expression<Func<CarDetailDto, bool>> filter = null)
         {
             using (RentACarContext context = new RentACarContext())
             {
+
                 var result = from c in context.Cars
-                    join b in context.Brands
-                        on c.BrandId equals b.Id
-                    join co in context.Colors
-                        on c.ColorId equals co.Id
-                    select new CarDetailDto
-                    {
-                        Id = c.Id,
-                        BrandId = c.BrandId,
-                        BrandName = b.Name,
-                        ColorId = c.ColorId,
-                        ColorName = co.Name,
-                        DailyPrice = c.DailyPrice,
-                        Description = c.Description,
-                        ModelYear = c.ModelYear
-                    };
-                return  filter==null
+                             join b in context.Brands
+                                 on c.BrandId equals b.Id
+                             join co in context.Colors
+                                 on c.ColorId equals co.Id
+                             select new CarDetailDto
+                             {
+                                 Id = c.Id,
+                                 BrandId = c.BrandId,
+                                 BrandName = b.Name,
+                                 ColorId = c.ColorId,
+                                 ColorName = co.Name,
+                                 DailyPrice = c.DailyPrice,
+                                 Description = c.Description,
+                                 ModelYear = c.ModelYear,
+                                 CarImages = ((from ci in context.CarImages
+                                              where (c.Id == ci.CarId)
+                                              select new CarImage
+                                              {
+                                                  Id = ci.Id,
+                                                  CarId = ci.CarId,
+                                                  Date = ci.Date,
+                                                  ImagePath = ci.ImagePath
+                                              }).ToList()).Count == 0
+                                                    ? new List<CarImage> { new CarImage { Id = -1, CarId = c.Id, Date = DateTime.Now, ImagePath = "/images/default.jpg" } }
+                                                    : (from ci in context.CarImages
+                                                       where (c.Id == ci.CarId)
+                                                       select new CarImage
+                                                       {
+                                                           Id = ci.Id,
+                                                           CarId = ci.CarId,
+                                                           Date = ci.Date,
+                                                           ImagePath = ci.ImagePath
+                                                       }).ToList()
+                             };
+                return filter == null
                 ? result.ToList()
                 : result.Where(filter).ToList();
             }
--- Entities/DTOs/CarDetailDto.cs
+++ Entities/DTOs/CarDetailDto.cs
@@ -1,5 +1,6 @@
-﻿using System;
+﻿using System.Collections.Generic;
 using Core.Entities;
+using Entities.Concrete;
 
 namespace Entities.DTOs
 {
@@ -13,5 +14,6 @@ namespace Entities.DTOs
         public int ModelYear { get; set; }
         public decimal DailyPrice { get; set; }
         public string Description { get; set; }
+        public List<CarImage> CarImages { get; set; }
     }
 }
